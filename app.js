2f319577bea15db52338a3fd0e782d9b8f934e61
beata <beata@emc2wave.com>
2019-09-20 17:42:21 -0700

    add loadEventListener

--- app.js
+++ app.js
@@ -4,3 +4,21 @@ const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-task');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// load all enevt listeners fn
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener('submit', addTask);
+}
+
+// add Task
+function addTask(e) {
+  if(taskInput.value === '') {
+    alert('Add a task');
+  }
+
+  e.preventDefault();
+}
